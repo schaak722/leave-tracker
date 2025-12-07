4b977168f1288e6e57aee6604ed0238c254eb650
schaak722 <js@schaak.mt>
2025-12-07 11:11:26 +0100

    Update config.py
    Email Notification Intro

--- config.py
+++ config.py
@@ -7,3 +7,12 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "change-this-secret-key-to-something-r
 ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
 ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me")
 
+# Outgoing mail settings (used for leave request notifications)
+MAIL_SERVER = os.environ.get("MAIL_SERVER")
+MAIL_PORT = int(os.environ.get("MAIL_PORT", "587"))
+MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS", "true").lower() == "true"
+MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+# Always send from the shared info@ address by default
+MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER", "info@keepmeposted.com.mt")
